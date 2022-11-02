06892800d4ccbb5730831756c9bb45578d335039
Hitarth Thanki <hitarth.thanki9006@gmail.com>
2022-11-02 15:19:57 -0500

    tuesdays code addition commit. yes its done on wed after. so what.

---
+++ Extras and notes/November/extra 11_1_1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+//! create a dynamic array of pointers to nodes.
+//! randomely generate a number
+//! every element of the array is a pointer to the node, and every number is added to the beginning of the ll.
+     //! every number's index is found by num % 10. this gives a dynamic array of 10 
+//! google the cashing techniques for your processors.
+
+//linear hashing quadratic probing. 
+//open chaining is on thursday for the first 10 mins. ?
+int main(int argc, char* argv[]){
+     
+     return 0;
+}

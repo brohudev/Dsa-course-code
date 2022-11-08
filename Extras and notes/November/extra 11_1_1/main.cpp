98187b957f6080e04c997001fee47e6f1311cb6f
Hitarth Thanki <hitarth.thanki9006@gmail.com>
2022-11-08 13:06:13 -0600

    11.08 commit for extras. nothing, just extra mains and stuff.

--- Extras and notes/November/extra 11_1_1/main.cpp
+++ Extras and notes/November/extra 11_1_1/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <algorithm>
 #include <fstream>
-
+#include <vector>
 using namespace std;
 //! create a dynamic array of pointers to nodes.
 //! randomely generate a number
@@ -13,6 +13,8 @@ using namespace std;
 //linear hashing quadratic probing. 
 //open chaining is on thursday for the first 10 mins. ?
 int main(int argc, char* argv[]){
+     vector<vector<int>> array;
+     int **array = new int*[10];
      
      return 0;
 }
---
+++ Extras and notes/November/extra 11_1_2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+     //!http://replit.com/@nouhad2007/treetracersal
+     //* postorder preorder and inorder traversal of heaps.
+
+     //* preorder traversal is basically the cout but before the recursion calls.
+     //* inorder is basically cout in between calls.
+     //* 
+     return 0;
+}
